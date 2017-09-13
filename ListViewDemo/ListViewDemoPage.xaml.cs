3a927a427bc9e912040142eed8b7809a92220bcb
vksuman <vksuman@nhbc.co.uk>
2017-09-13 14:42:24 +0100

    Initial check-in of module ListViewDemo

---
+++ ListViewDemo/ListViewDemoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace ListViewDemo
+{
+	public partial class ListViewDemoPage : ContentPage
+	{
+		public ListViewDemoPage()
+		{
+			InitializeComponent();
+		}
+		void OnClicked(object sender, System.EventArgs e)
+		{
+			Navigation.PushAsync(new IntractiveListView());
+		}
+	}
+}
